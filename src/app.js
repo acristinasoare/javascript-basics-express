f442988f85b0a968ddf5c0bb4307daa1ecfa2512
Cristina Soare <kiti.soare@ymail.com>
2023-06-18 21:07:34 +0100

    passed first numbers test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const {
   sayHello,
   uppercase,
@@ -7,7 +8,11 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.string) });
@@ -29,6 +34,10 @@ app.get('/strings/first-characters/:string', (req, res) => {
   } else {
     res.status(200).json({ result: firstCharacter(string) });
   }
+});
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  res.status(200).json({ result: add(parseInt(req.params.a), parseInt(req.params.b)) });
 });
 
 module.exports = app;
